46b565372405fb6f41c5a918620c7d5adabbdd4b
JakubNovvak <jakubszymonnowak@gmail.com>
2024-02-13 15:49:08 +0100

    Add TutorNet logo on the NavigationBar

--- Source/Client/TutorNet.Client.GUI/src/components/NavigationBar/NavigationBar.tsx
+++ Source/Client/TutorNet.Client.GUI/src/components/NavigationBar/NavigationBar.tsx
@@ -1,5 +1,13 @@
-import { Toolbar, Box, AppBar } from '@mui/material';
+import { Toolbar, Box, AppBar, styled } from '@mui/material';
 import reactLogo from './assets/react.svg';
+import TutorNetLogo from '../../assets/TutorNetLogo.png';
+
+const AnimatedImage = styled("img")({
+    transition: 'all 0.3 ease-in-out',
+    '&:hover': {
+        transform: 'scale(1.1)'
+    }
+});
 
 const navColor = "#201c1c";
 
@@ -9,7 +17,7 @@ export default function NavigationBar(){
             <Box sx={{flex: 1}}>
                 <AppBar sx={{ background: navColor}} position="static">
                     <Toolbar sx={{ marginTop: "5px", marginBottom: "5px"}}>
-
+                        <AnimatedImage src={TutorNetLogo} style={{height: "45px"}}></AnimatedImage>
                     </Toolbar>
                 </AppBar>
             </Box>
