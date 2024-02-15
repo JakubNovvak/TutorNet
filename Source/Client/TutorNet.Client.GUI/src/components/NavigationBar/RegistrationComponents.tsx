4c3c856c162380ec532e24c14d717c4ebf11c451
JakubNovvak <jakubszymonnowak@gmail.com>
2024-02-15 17:15:28 +0100

    Change Online Registration border color

--- Source/Client/TutorNet.Client.GUI/src/components/NavigationBar/RegistrationComponents.tsx
+++ Source/Client/TutorNet.Client.GUI/src/components/NavigationBar/RegistrationComponents.tsx
@@ -5,7 +5,7 @@ export default function RegistrationComponents(){
     return (
         <>
             <Box marginLeft="auto">
-                <Button variant='outlined' style={{color: "white", borderColor: "#a3a3a3"}}>Online Registration</Button>
+                <Button variant='outlined' style={{color: "white", borderColor: "#4bb7f8"}}>Online Registration</Button>
                 <Button variant='outlined' style={{marginLeft: '20px', color: "white", borderColor: "#a3a3a3"}} >Call Us</Button>
             </Box>
         </>
