de7d51a15015e8a0a632eb3f0e219023d37f55e7
JakubNovvak <jakubszymonnowak@gmail.com>
2024-02-15 13:38:43 +0100

    Create new directory for Carousel components of the Home Page and update imports

---
+++ Source/Client/TutorNet.Client.GUI/src/components/HomePage/Statistics.tsx
@@ -0,0 +1,13 @@
+import { styled } from "@mui/material";
+
+const Container = styled("div")({
+    minHeight: "580px"
+  });
+
+export default function Statistics(){
+    return(
+        <Container>
+            
+        </Container>
+    );
+}
