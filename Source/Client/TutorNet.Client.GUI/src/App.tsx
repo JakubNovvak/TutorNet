c46b0f6660013a130fb22d7e40a01c5e34592e53
JakubNovvak <jakubszymonnowak@gmail.com>
2024-02-18 22:37:29 +0100

    Prepare FinalizeReservationPage View, with working Form

--- Source/Client/TutorNet.Client.GUI/src/App.tsx
+++ Source/Client/TutorNet.Client.GUI/src/App.tsx
@@ -4,8 +4,9 @@ import './App.css';
 import Box from '@mui/material/Box';
 import NavigationBar from './components/NavigationBar/NavigationBar';
 import Footer from './components/Footer/Footer';
-//import HomePage from './pages/Home/HomePage';
+import HomePage from './pages/Home/HomePage';
 import ReservationPage from "./pages/Reservation/ReservationPage";
+import FinalizeReservationPage from "./pages/FinalizeReservation/FinalizeReservationPage";
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -15,18 +16,19 @@ function App() {
       <NavigationBar />
       <Box
         sx={{
-          //minHeight: "calc(100vh - 235px)",
-          minHeight: "100vh",
+          minHeight: "calc(100vh - 235px)",
+          //minHeight: "100vh",
           //backgroundColor: "#e8e8e8",
           backgroundColor: "white"
         }}
       >
         {/* TODO: Here will be the rest od the content */}
         {/* <HomePage /> */}
-        <ReservationPage/>
+        {/* <ReservationPage/> */}
+        <FinalizeReservationPage/>
       </Box>
       {/* {TODO: Proper implementation fo that spacing below} */}
-      <Box height={150} bgcolor="white"></Box>
+      {/* <Box height={150} bgcolor="white"></Box> */}
       <Footer />
     </>
   )
---
+++ Source/Client/TutorNet.Client.GUI/src/components/FinalizeReservationPage/GenericFormInput.tsx
@@ -0,0 +1,51 @@
+import { FormControl, FormLabel, Grid, styled } from "@mui/material";
+import Input from '@mui/joy/Input';
+import { FormikContextType } from "formik";
+import React, { useState, useEffect} from "react";
+import { FormValues, FormikOnBlurHandler, FormikOnChangeHandler } from "./FormValues";
+
+const GridItem = styled(Grid)({
+    paddingTop: "1.3rem",
+    paddingBottom: "1.3rem"
+})
+
+//Testing
+
+const LabelsArray: string[] = [
+    "Reservation Date", 
+    "E-mail Address", 
+    "Name | Surname", 
+    "Material Range", 
+    "Address", 
+    "Phone Number", 
+    "Reservation Comment"
+];
+
+const placeholdersArray: string[] = [
+    "12 February 12-13", 
+    "example@example.com", 
+    "Jan Kowalski", 
+    "Primary School - 6 grade", 
+    "Test, Testowa 15A", 
+    "+48 123 456 789", 
+    "Reservation Comment"
+];
+
+//Testing
+
+export default function GenericFormButton({ InputNumber, Size, idName, FormikValue, FormikValueOnChange, FormikOnBlur }: 
+    {InputNumber : number, Size: number, idName: string, FormikValue: string, FormikValueOnChange: FormikOnChangeHandler, FormikOnBlur: FormikOnBlurHandler}): 
+    JSX.Element {
+
+    return(
+        <GridItem item xs={Size}>
+            <FormControl>
+                <FormLabel sx={{marginRight: "auto", fontSize: "0.8rem", marginBottom: "-0.0rem"}}>{LabelsArray[InputNumber]}</FormLabel>
+                <Input className="outlined-input" id={idName} placeholder={placeholdersArray[InputNumber]}  disabled={InputNumber == 0 ? true : false} 
+                 value={FormikValue} onChange={FormikValueOnChange} onBlur={FormikOnBlur}
+                 sx={{minWidth: InputNumber == 6 ? "40rem" : "21rem"}}
+                 />
+            </FormControl>
+        </GridItem>
+    );
+}
