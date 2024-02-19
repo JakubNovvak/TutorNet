5b2f0716aab1d9bf6aaf1c6ab2e47dfb3849f018
JakubNovvak <jakubszymonnowak@gmail.com>
2024-02-19 13:23:41 +0100

    Add initial database files - PrepDb preparing population and AppDbContext

---
+++ Source/Server/TutorNet.Server.API/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TutorNet.Server.API.Models;
+
+namespace TutorNet.Server.API.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Tutor> Tutors { get; set; }
+        public DbSet<Lesson> Lessons { get; set; }
+    }
+}
---
+++ Source/Server/TutorNet.Server.API/Data/PrepDb.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics.Eventing.Reader;
+using TutorNet.Server.API.Models;
+
+namespace TutorNet.Server.API.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder app, bool isProduction)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                if (isProduction)
+                    SeedDataProduction(serviceScope.ServiceProvider.GetService<AppDbContext>());
+                else
+                    SeedDataDevelopment(serviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
+
+        private static void SeedDataProduction(AppDbContext? dbContext)
+        {
+            if (dbContext == null)
+                throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        private static void SeedDataDevelopment(AppDbContext? dbContext)
+        {
+            if (dbContext == null)
+                throw new ArgumentNullException(nameof(dbContext));
+
+            if (!dbContext.Lessons.Any())
+            {
+                //TODO: Implement database seeding
+            }
+            else
+                Console.WriteLine(">[DbSeed] Lessons Table already has data.");
+
+            if (!dbContext.Tutors.Any())
+            {
+                Console.WriteLine(">[DbSeed] Seeding Data...");
+                //TODO: Implement database seeding
+                dbContext.AddRange(
+                    new Tutor() { Name = "Jan", Surname = "Kowalski", Email = "jankowalski@test.pl", Subject = "Matematyka", Pricing = 50 }
+                    );
+
+                dbContext.SaveChanges();
+            }
+            else
+                Console.WriteLine(">[DbSeed] Tutors Table already has data.");
+        }
+    }
+}
