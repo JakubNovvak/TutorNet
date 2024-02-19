7ec41a81bdfa32e0f27c0f2dd0fd36f85ef70417
JakubNovvak <jakubszymonnowak@gmail.com>
2024-02-19 13:23:32 +0100

    Add initial version of the models

---
+++ Source/Server/TutorNet.Server.API/Models/Lesson.cs
@@ -0,0 +1,14 @@
+﻿namespace TutorNet.Server.API.Models
+{
+    public class Lesson
+    {
+        public int Id { get; set; }
+        public DateTime ReservationDate { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+        public int PhoneNumber { get; set; }
+        public string? ReservationComment { get; set; }
+        public int TutorId { get; set; }
+    }
+}
---
+++ Source/Server/TutorNet.Server.API/Models/Tutor.cs
@@ -0,0 +1,13 @@
+﻿namespace TutorNet.Server.API.Models
+{
+    public class Tutor
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Email { get; set; }
+        public string? Subject { get; set; }
+        public int Pricing { get; set; }
+        public ICollection<Lesson>? Lessons { get; set; } = new List<Lesson>();
+    }
+}
