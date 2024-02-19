cd3173d7711d747b26381a934365fcf7785590f9
JakubNovvak <jakubszymonnowak@gmail.com>
2024-02-19 15:32:26 +0100

    Create simple DTOs

---
+++ Source/Server/TutorNet.Server.API/Dtos/CalendarEntryReadDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static TutorNet.Server.API.Models.CalendarEntryTypes;
+
+namespace TutorNet.Server.API.Dtos
+{
+    public class CalendarEntryReadDto
+    {
+        public int Id { get; set; }
+        public CalendarEntryType CalendarEntryType { get; set; }
+        public bool IsAccepted { get; set; }
+        public DateTime ReservationDate { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+        public int PhoneNumber { get; set; }
+        public string? ReservationComment { get; set; }
+        public int TutorId { get; set; }
+    }
+}
