82e2675baee02ccdb780487b8d18db9be962f748
JakubNovvak <jakubszymonnowak@gmail.com>
2024-02-20 11:39:16 +0100

    Add TutorReadDto model and AutoMapper profile

---
+++ Source/Server/TutorNet.Server.API/Dtos/TutorReadDto.cs
@@ -0,0 +1,12 @@
+﻿namespace TutorNet.Server.API.Models
+{
+    public class TutorReadDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Email { get; set; }
+        public string? Subject { get; set; }
+        public int Pricing { get; set; }
+    }
+}
